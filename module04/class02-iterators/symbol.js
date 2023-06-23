094473147999e996a9b6db6407c1a2f9d5b5d8f5
Guilherme Behs <guilhermebehs@192.168.68.106>
2023-06-23 16:18:28 -0300

    feat: class 02 of module 04

--- module04/class02-iterators/symbol.js
+++ module04/class02-iterators/symbol.js
@@ -11,4 +11,26 @@ user[uniqueKey] = 'value for symbol'
 assert.deepStrictEqual(user['userName'], 'value for normal Objects')
 assert.deepStrictEqual(user[uniqueKey], 'value for symbol')
 assert.deepStrictEqual(user[Symbol('userName')], undefined)
-assert()
+
+const datesKey = Symbol('dates')
+const joinDatesMethod = Symbol('joinDates')
+
+class MyDate{  
+     
+    constructor(...args){
+        this[datesKey] = args.map(arg=> new Date(...arg).toISOString())
+    }
+
+    getDates(){
+        return this[joinDatesMethod]()
+    }
+
+    [joinDatesMethod](){
+        return this[datesKey].join(',')
+    }
+}
+
+const myDate = new MyDate([2021,1,10], [2020,1,10])
+assert.deepStrictEqual(myDate.mykey, undefined)
+assert.deepStrictEqual(myDate.joinDates, undefined)
+assert.deepStrictEqual(myDate.getDates(), '2021-02-10T03:00:00.000Z,2020-02-10T03:00:00.000Z')
